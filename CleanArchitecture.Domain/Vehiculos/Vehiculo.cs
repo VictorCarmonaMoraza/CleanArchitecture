fa7b84c136b7028b8421b467f2fbbf9fe761eb00
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-15 19:25:34 +0200

    8-Domain Modecon_Object_Values

--- CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
+++ CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -13,13 +13,9 @@ namespace CleanArchitecture.Domain.Vehiculos
         {
         }
 
-        public string? Modelo { get; private set; }
-        public string? Vin { get; private set; }
-        public string? Calle { get; private set; }
-        public string? Departamento { get; private set; }
-        public string? Provincia { get; private set; }
-        public string? Ciudad { get; private set; }
-        public string? Pais { get; private set; }
+        public Modelo? Modelo { get; private set; }
+        public Vin? Vin { get; private set; }
+        public Direccion? Direccion { get; private set; }
         public decimal Precio { get; private set; }
         public string? TipoMoneda { get; private set; }
         public string? Mantenimiento { get; private set; }
