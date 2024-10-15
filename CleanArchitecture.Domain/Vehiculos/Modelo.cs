6fcc1fbc289fc72926cc0416104a7b63db7081f0
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-15 20:06:43 +0200

    9-Modelo Enriquecido en DDD

--- CleanArchitecture.Domain/Vehiculos/Modelo.cs
+++ CleanArchitecture.Domain/Vehiculos/Modelo.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CleanArchitecture.Domain.Vehiculos;
 
-namespace CleanArchitecture.Domain.Vehiculos
-{
-    public record Modelo(string Value);
-}
+public record Modelo(string Value);
--- CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
+++ CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -1,26 +1,35 @@
 ﻿using CleanArchitecture.Domain.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CleanArchitecture.Domain.Vehiculos
+namespace CleanArchitecture.Domain.Vehiculos;
+
+public sealed class Vehiculo : Entity
 {
-    public sealed class Vehiculo : Entity
+    public Vehiculo(
+        Guid id,
+        Modelo? modelo,
+        Vin? vin,
+        Direccion? direccion,
+        Moneda? precio,
+        Moneda? mantenimiento,
+        DateTime? fechaUltimoAlquiler,
+        List<Accesorio> accesorios
+        ) : base(id)
     {
-        public Vehiculo(Guid id) : base(id)
-        {
-        }
-
-        public Modelo? Modelo { get; private set; }
-        public Vin? Vin { get; private set; }
-        public Direccion? Direccion { get; private set; }
-        public decimal Precio { get; private set; }
-        public string? TipoMoneda { get; private set; }
-        public string? Mantenimiento { get; private set; }
-        public string? MantenimientoTipoMoneda { get; private set; }
-        public DateTime? FechaUltimoAlquiler { get; private set; }
-        public List<Accesorio> Accesorios { get; private set; } = new();
+        Modelo = modelo;
+        Vin = vin;
+        Direccion = direccion;
+        Precio = precio;
+        Mantenimiento = mantenimiento;
+        FechaUltimoAlquiler = fechaUltimoAlquiler;
+        Accesorios = accesorios;
     }
+
+    public Modelo? Modelo { get; private set; }
+    public Vin? Vin { get; private set; }
+    public Direccion? Direccion { get; private set; }
+    public Moneda? Precio { get; private set; }
+    public Moneda? Mantenimiento { get; private set; }
+    public DateTime? FechaUltimoAlquiler { get; private set; }
+    public List<Accesorio> Accesorios { get; private set; } = new();
 }
+
