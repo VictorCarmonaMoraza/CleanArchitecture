8e79fd1ca0fddd78354001a8e03626fd6565033d
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-16 19:52:42 +0200

    13-Modelamiento de Entity

---
+++ CleanArchitecture.Domain/Alquileres/Alquiler.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Alquileres.Enum;
+
+namespace CleanArchitecture.Domain.Alquileres;
+
+public sealed class Alquiler : Entity
+{
+    public Alquiler(Guid id) : base(id)
+    {
+    }
+
+    public AlquilerStatus Status { get; private set; }
+
+
+}
---
+++ CleanArchitecture.Domain/Alquileres/Enum/AlquilerStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Alquileres.Enum;
+
+public enum AlquilerStatus
+{
+    Reservado = 1,
+    Confirmado = 2,
+    Rechazado = 3,
+    Cancelado = 4,
+    Finalizado = 5
+}
