897c0eecb7a04d4baf32383a56f73f34d9e50891
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-16 19:31:46 +0200

    12-Patron Repositoy y UnitOfwork

---
+++ CleanArchitecture.Domain/Users/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Users;
+
+public interface IUserRepository
+{
+    //Obtener usuario por Id
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellation = default);
+
+    //Crear un usuario
+    void Add(User user);
+}
---
+++ CleanArchitecture.Domain/Vehiculos/IVehiculoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Vehiculos;
+
+public interface IVehiculoRepository
+{
+    //Busqueda de un vehiculo por su id
+    Task<Vehiculo?> GetByIdAsync(Guid id, CancellationToken cancellation = default);
+}
